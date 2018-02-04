bbe3f1d9c1747157d6b0265a28df47bc2c316de6
Chris Atkin <atkinchris@gmail.com>
2018-02-04 17:34:55 +0000

    Draw regular polygon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
+const TWO_PI = Math.PI * 2
+
 const canvas = document.getElementById('canvas')
+
+const rPoly = (x, y, radius, nsides) => {
+  const points = []
+  const angle = TWO_PI / nsides
+
+  for (let a = 0; a < TWO_PI; a += angle) {
+    points.push({
+      x: x + (Math.cos(a) * radius),
+      y: y + (Math.sin(a) * radius),
+    })
+  }
+
+  return points
+}
 
 const sketch = (p) => {
   const RED = '#ec3637'
+  const polygon = rPoly(0, 0, 100, 10)
 
   p.setup = () => {
     p.createCanvas(canvas.offsetWidth, canvas.offsetHeight)
@@ -14,7 +31,12 @@ const sketch = (p) => {
   p.draw = () => {
     p.translate(p.width / 2, p.height / 2)
     p.fill(RED)
-    p.ellipse(0, 0, 100, 100)
+
+    p.beginShape()
+
+    polygon.forEach(v => p.vertex(v.x, v.y))
+
+    p.endShape()
   }
 }
 
