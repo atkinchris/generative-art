d2e4651d393fbdc6057bac471ff0938296c59838
Chris Atkin <atkinchris@gmail.com>
2018-02-04 15:40:51 +0000

    Create README.md

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-function setup() {
+const canvas = document.getElementById('canvas')
 
+const sketch = (p) => {
+  const RED = '#ec3637'
+
+  p.setup = () => {
+    p.createCanvas(canvas.offsetWidth, canvas.offsetHeight)
+    p.noStroke()
+    p.colorMode(p.HSB)
+    p.blendMode(p.MULTIPLY)
+    p.noLoop()
+  }
+
+  p.draw = () => {
+    p.translate(p.width / 2, p.height / 2)
+    p.fill(RED)
+    p.ellipse(0, 0, 100, 100)
+  }
 }
 
-function draw() {
-  ellipse(50, 50, 80, 80)
-}
-
-global.setup = setup
-global.draw = draw
+new p5(sketch, canvas) // eslint-disable-line no-new
