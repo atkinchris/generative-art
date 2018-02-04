c487bdbd932f96eb90d501c581b89a04bc718c95
Chris Atkin <atkinchris@gmail.com>
2018-02-04 13:45:10 +0000

    Initial commit of dependencies and structure

---
+++ browsersync.js
@@ -0,0 +1,8 @@
+module.exports = {
+  server: 'src',
+  files: 'src',
+  port: 8080,
+  ui: false,
+  notify: false,
+  open: false,
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+function setup() {
+
+}
+
+function draw() {
+  ellipse(50, 50, 80, 80)
+}
+
+global.setup = setup
+global.draw = draw
