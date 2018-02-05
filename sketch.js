59b4dfeb5aa609984acc68f9bf2816ea4823c20b
Chris Atkin <atkinchris@gmail.com>
2018-02-05 21:23:18 +0000

    Draw perlin noise to 2D canvas

--- sketch.js
+++ sketch.js
@@ -1,102 +1,30 @@
 const canvas = document.getElementById('canvas')
 
 const sketch = (p) => {
-  const rPolygon = (x, y, radius, nsides) => {
-    const points = []
-    const angle = p.TWO_PI / nsides
-
-    for (let a = 0; a < p.TWO_PI; a += angle) {
-      points.push({
-        x: x + (Math.cos(a) * radius),
-        y: y + (Math.sin(a) * radius),
-        z: p.random(-1, 1),
-      })
-    }
-
-    return points
-  }
-
-  const moveNearby = (point, deviation) => ({
-    x: p.randomGaussian(point.x, point.z * deviation),
-    y: p.randomGaussian(point.y, point.z * deviation),
-    z: point.z,
-  })
-
-  const deform = (points, deviation) => points.map(point => moveNearby(point, deviation))
-
-  const interpolate = (points, iterations = 1) => {
-    const newPoints = []
-
-    for (let iteration = 0; iteration < iterations; iteration += 1) {
-      for (let i = 0; i < points.length; i += 1) {
-        const p1 = points[i]
-        const p2 = points[(i + 1) % points.length]
-        const pMid = {
-          x: (p1.x + p2.x) / 2,
-          y: (p1.y + p2.y) / 2,
-          z: ((p1.z + p2.z) / 2) * 0.55 * p.random(0.5, 2.5),
-        }
-
-        newPoints.push(p1)
-        newPoints.push(pMid)
-      }
-    }
-
-    return newPoints
-  }
-
-  const drawPoly = (points) => {
-    p.beginShape()
-    points.forEach(v => p.vertex(v.x, v.y))
-    p.endShape(p.CLOSE)
-  }
-
-  const drawDeformedPoly = (radius, offset, colour) => {
-    const randomRadius = p.randomGaussian(radius, radius / 10)
-    const baseShape = rPolygon(0, 0, randomRadius, 10)
-    let polygon = baseShape
-
-    const fill = p.color(colour)
-    fill.setAlpha(0.04)
-    p.fill(fill)
-
-    p.translate(p.width / 2, p.height / 2)
-    p.translate(offset.x, offset.y)
-
-    const iterations = 3
-    for (let iteration = 0; iteration < iterations; iteration += 1) {
-      polygon = interpolate(polygon)
-      polygon = deform(polygon, 5)
-    }
-
-    const layers = 30
-    const deformations = 5
-    for (let layer = 0; layer < layers; layer += 1) {
-      let current = polygon
-      for (let deformation = 0; deformation < deformations; deformation += 1) {
-        current = deform(current, 8)
-      }
-      drawPoly(current)
-    }
-
-    p.resetMatrix()
-  }
-
-
   p.setup = () => {
-    p.createCanvas(canvas.offsetWidth, canvas.offsetHeight)
+    p.createCanvas(window.innerWidth, window.innerWidth)
     p.noStroke()
-    p.colorMode(p.HSB)
+    p.colorMode(p.RGB)
     p.blendMode(p.MULTIPLY)
     p.noLoop()
+    p.pixelDensity(1)
   }
 
   p.draw = () => {
-    const radius = 100
-    const offset = radius * 0.4
+    p.loadPixels()
+    for (let y = 0; y < p.height; y += 1) {
+      for (let x = 0; x < p.width; x += 1) {
+        const r = p.noise(x * 0.01, y * 0.01) * 255
+        const i = ((y * p.width) + x) * 4
 
-    drawDeformedPoly(radius, { x: -offset, y: 0 }, '#E0F9B5')
-    drawDeformedPoly(radius, { x: offset, y: 0 }, '#A5DEE5')
+
+        p.pixels[i + 0] = r
+        p.pixels[i + 1] = r
+        p.pixels[i + 2] = r
+        p.pixels[i + 3] = 255
+      }
+    }
+    p.updatePixels()
   }
 }
 
