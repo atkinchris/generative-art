a350f9c7b1931089139f9bd1cb97c062bf8285be
Chris Atkin <atkinchris@gmail.com>
2018-02-06 20:00:36 +0000

    Adjust field angles for a general heading

--- flow-field/field.js
+++ flow-field/field.js
@@ -1,15 +1,19 @@
 const buildField = (p, { width, height, scale = 20 }) => {
   const field = []
 
+  const angleHeading = -45
+  const angleScale = 0.25
+  const noiseScale = 0.01 * scale
   const widthScaled = Math.floor(width / scale)
   const heightScaled = Math.floor(height / scale)
-  const noiseScale = 0.008 * scale
 
   for (let y = 0; y < heightScaled; y += 1) {
     for (let x = 0; x < widthScaled; x += 1) {
       const index = (widthScaled * y) + x
-      const r = p.noise(-x * noiseScale, y * noiseScale)
-      const vector = p5.Vector.fromAngle(r * p.TWO_PI)
+      const r = p.noise(x * noiseScale, y * noiseScale)
+      const angle = p.radians((r * angleScale * 360) + (-45 + angleHeading))
+      const vector = p5.Vector.fromAngle(angle)
+      vector.setMag(0.01)
 
       field[index] = vector
     }
@@ -23,17 +27,20 @@ const buildField = (p, { width, height, scale = 20 }) => {
     const length = 0.5 * scale
     p.push()
 
+    p.stroke(50, 0.5)
+    p.fill(50, 0.5)
+    p.strokeWeight(length * 0.2)
+
     p.translate(canvasX, canvasY)
     p.rotate(vector.heading())
-    p.strokeWeight(length * 0.2)
     p.line(0, 0, length, 0)
     p.triangle(
       length / 2,
-      length * 0.3,
+      length * 0.2,
       length,
       0,
       length / 2,
-      length * -0.3,
+      length * -0.2,
     )
 
     p.pop()
