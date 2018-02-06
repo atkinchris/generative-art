407a2f7fa76b0ae6d09378c08690729af6860080
Chris Atkin <atkinchris@gmail.com>
2018-02-06 13:53:51 +0000

    Refactor vectors and flow field

--- flow-field/sketch.js
+++ flow-field/sketch.js
@@ -1,11 +1,27 @@
 const canvas = document.getElementById('canvas')
 
 const sketch = (p) => {
+  const width = 40
+  const height = 40
   const scale = 10
-  const noiseScale = 0.01
+  const noiseScale = 0.01 * scale
+  const flowField = []
+
+  const drawVector = (vector, x, y) => {
+    const canvasX = (x * scale) + (scale / 2)
+    const canvasY = (y * scale) + (scale / 2)
+    const length = 0.5 * scale
+    p.push()
+
+    p.translate(canvasX, canvasY)
+    p.rotate(vector.heading())
+    p.line(0, 0, length, 0)
+
+    p.pop()
+  }
 
   p.setup = () => {
-    p.createCanvas(400, 400)
+    p.createCanvas(width * scale, height * scale)
     // p.noStroke()
     p.colorMode(p.RGB)
     p.blendMode(p.MULTIPLY)
@@ -14,21 +30,14 @@ const sketch = (p) => {
   }
 
   p.draw = () => {
-    for (let y = 0; y < p.height; y += scale) {
-      for (let x = 0; x < p.width; x += scale) {
-        const r = p.noise(
-          x * noiseScale,
-          y * noiseScale,
-        )
+    for (let y = 0; y < height; y += 1) {
+      for (let x = 0; x < width; x += 1) {
+        const index = (width * y) + x
+        const r = p.noise(x * noiseScale, y * noiseScale)
         const vector = p5.Vector.fromAngle(r * p.TWO_PI)
 
-        p.push()
-
-        p.translate(x + (scale / 2), y + (scale / 2))
-        p.rotate(vector.heading())
-        p.line(0, 0, scale / 2, 0)
-
-        p.pop()
+        flowField[index] = vector
+        drawVector(vector, x, y)
       }
     }
   }
