07367fabb2dfdb6307eafb0a5bfbe3b113f96d38
Chris Atkin <atkinchris@gmail.com>
2018-02-09 19:47:29 +0000

    Add subdivision of triangles

--- experiments/triangle-subdivision/sketch.js
+++ experiments/triangle-subdivision/sketch.js
@@ -3,21 +3,58 @@ const canvas = document.querySelector('.container')
 const sketch = (p) => {
   const width = 400
   const height = 400
+  const stack = []
+
+  const distance = points => Math.sqrt((
+    ((points[2] - points[0]) ** 2) + ((points[3] - points[1]) ** 2)
+  ))
+
+  const drawTriangle = (points) => {
+    if (distance(points) < p.randomGaussian(width / 10, width / 20)) {
+      return
+    }
+
+    p.beginShape()
+    p.vertex(points[0], points[1])
+    p.vertex(points[2], points[3])
+    p.vertex(points[4], points[5])
+    p.endShape(p.CLOSE)
+
+    const midPoint = [
+      (points[0] + points[2]) / 2,
+      (points[1] + points[3]) / 2,
+    ]
+
+    stack.push([
+      points[4], points[5],
+      points[0], points[1],
+      midPoint[0], midPoint[1],
+    ])
+
+    stack.push([
+      points[4], points[5],
+      points[2], points[3],
+      midPoint[0], midPoint[1],
+    ])
+  }
 
   p.setup = () => {
     p.createCanvas(width, height)
     p.colorMode(p.HSB)
     p.blendMode(p.MULTIPLY)
     p.noFill()
-    p.noLoop()
+
+    stack.push([0, 0, width, height, width, 0])
+    stack.push([0, 0, width, height, 0, height])
   }
 
   p.draw = () => {
-    p.beginShape()
-    p.vertex(0, 0)
-    p.vertex(width, height)
-    p.vertex(0, height)
-    p.endShape(p.CLOSE)
+    if (stack.length > 0) {
+      drawTriangle(stack.shift())
+      drawTriangle(stack.pop())
+    } else {
+      p.noLoop()
+    }
   }
 }
 
