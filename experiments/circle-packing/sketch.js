cc55820aaad9748289e46325ef74545c0dcc51f9
Chris Atkin <atkinchris@gmail.com>
2018-02-10 17:03:46 +0000

    Improve packing and colours

--- experiments/circle-packing/sketch.js
+++ experiments/circle-packing/sketch.js
@@ -9,19 +9,28 @@ const sketch = (p) => {
     y: height / 2,
   }
   const canvasRadius = width / 2
-  const config = [
-    { max: 15, radius: 30, colour: '#48466D' },
-    { max: 50, radius: 20, colour: '#3D84A8' },
-    { max: 200, radius: 10, colour: '#46CDCF' },
-    { max: undefined, radius: 5, colour: '#ABEDD8' },
+  const groups = [
+    { radius: 30, saturation: 0.2, max: 20 },
+    { radius: 20, saturation: 0.3, max: 50 },
+    { radius: 10, saturation: 0.4, max: 200 },
+    { radius: 5, saturation: 0.5 },
   ]
+  const DEFAULT_HUE = 200
+  const DEFAULT_BRIGHTNESS = 96
 
   const distance = (a, b) => Math.sqrt((
     ((b.x - a.x) ** 2) + ((b.y - a.y) ** 2)
   ))
 
-  const packCircles = ({ radius, colour, max = Number.MAX_SAFE_INTEGER }) => {
-    let retries = 2000
+  const packCircles = (config) => {
+    const {
+      radius,
+      hue = DEFAULT_HUE,
+      saturation,
+      brightness = DEFAULT_BRIGHTNESS,
+      max = Number.MAX_SAFE_INTEGER,
+    } = config
+    let retries = 20000
     let count = 0
 
     while (count < max && retries > 0) {
@@ -29,10 +38,12 @@ const sketch = (p) => {
         x: p.random(radius, width - radius),
         y: p.random(radius, height - radius),
         radius,
-        colour,
+        hue,
+        saturation,
+        brightness,
       }
       const test = circle => distance(circle, newCircle) < (radius + circle.radius) / 2
-      const withinCanvas = distance(center, newCircle) < canvasRadius
+      const withinCanvas = distance(center, newCircle) < canvasRadius - (radius / 2)
 
       if (withinCanvas && !circles.some(test)) {
         circles.push(newCircle)
@@ -49,12 +60,16 @@ const sketch = (p) => {
     p.noStroke()
     p.noLoop()
 
-    config.forEach(packCircles)
+    groups.forEach(packCircles)
   }
 
   p.draw = () => {
     circles.forEach((circle) => {
-      p.fill(circle.colour)
+      p.fill(
+        circle.hue,
+        circle.saturation * 255,
+        circle.brightness,
+      )
       p.ellipse(circle.x, circle.y, circle.radius)
     })
   }
