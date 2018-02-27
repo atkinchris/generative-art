bbfc3e4ebcfed66114c6ef52fd362066089b3ae0
Chris Atkin <atkinchris@gmail.com>
2018-02-27 11:57:48 +0000

    Improve shading

--- src/isometric/isoCube.js
+++ src/isometric/isoCube.js
@@ -7,10 +7,15 @@ const drawIsoCube = (ctx, { x, y, size, angle = 30, colour }) => {
 
   const moveTo = (rX, rY) => ctx.moveTo(x + rX, y + rY)
   const lineTo = (rX, rY) => ctx.lineTo(x + rX, y + rY)
+  const gradTo = (x1, y1, x2, y2) => ctx.createLinearGradient(x + x1, y + y1, x + x2, y + y2)
 
   const rFace = () => {
     ctx.strokeStyle = 'rgba(255, 255, 255, 0.25)'
-    ctx.fillStyle = `hsl(${colour}, 50%, 60%)`
+
+    const gradient = gradTo(0, 0, dX, dY)
+    gradient.addColorStop(0, `hsl(${colour}, 50%, 70%)`)
+    gradient.addColorStop(1, `hsl(${colour}, 50%, 50%)`)
+    ctx.fillStyle = gradient
 
     ctx.beginPath()
     moveTo(0, 0)
@@ -24,7 +29,11 @@ const drawIsoCube = (ctx, { x, y, size, angle = 30, colour }) => {
 
   const lFace = () => {
     ctx.strokeStyle = 'rgba(255, 255, 255, 0.25)'
-    ctx.fillStyle = `hsl(${colour}, 50%, 40%)`
+
+    const gradient = gradTo(0, 0, -dX, dY)
+    gradient.addColorStop(0, `hsl(${colour}, 50%, 50%)`)
+    gradient.addColorStop(1, `hsl(${colour}, 50%, 30%)`)
+    ctx.fillStyle = gradient
 
     ctx.beginPath()
     moveTo(0, 0)
