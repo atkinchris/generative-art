d3a5106be028a3ad1b025b89c654a9f76bbe07c4
Chris Atkin <atkinchris@gmail.com>
2018-03-05 12:37:13 +0000

    Draw 3D cube

--- src/terrariums/index.js
+++ src/terrariums/index.js
@@ -4,33 +4,72 @@ const ctx = canvas.getContext('2d')
 
 container.appendChild(canvas)
 
+const sortMax = (prop, desc) => (a, b) => {
+  const max = x => Math.max(...x.map(v => v[prop]))
+  return desc ? max(a) < max(b) : max(a) > max(b)
+}
+
 const draw = () => {
   const width = 400
   const height = 400
   canvas.width = width
   canvas.height = height
 
-  const leafPolygon = () => [
-    { x: 30, y: 0 },
-    { x: 50, y: 30, cX: 50, cY: 0 },
-    { x: 30, y: 100, cX: 50, cY: 65 },
-    { x: 10, y: 30, cX: 10, cY: 65 },
-    { x: 30, y: 0, cX: 10, cY: 0 },
+  const size = canvas.width / 2
+
+  const vertices = [
+    { x: -1.0, y: -1.0, z: -1.0 }, // Front-Bottom-Left
+    { x: 1.0, y: -1.0, z: -1.0 }, // Front-Bottom-Right
+    { x: -1.0, y: -1.0, z: 1.0 }, // Rear-Bottom-Left
+    { x: 1.0, y: -1.0, z: 1.0 }, // Rear-Bottom-Right
+    { x: -1.0, y: 1.0, z: -1.0 }, // Front-Top-Left
+    { x: 1.0, y: 1.0, z: -1.0 }, // Front-Top-Right
+    { x: -1.0, y: 1.0, z: 1.0 }, // Rear-Top-Left
+    { x: 1.0, y: 1.0, z: 1.0 }, // Rear-Top-Right
   ]
 
-  const drawPoly = (points) => {
+  const faces = [
+    [vertices[0], vertices[1], vertices[5], vertices[4]], // Front
+    [vertices[2], vertices[3], vertices[7], vertices[6]], // Rear
+    [vertices[0], vertices[1], vertices[3], vertices[2]], // Bottom
+    [vertices[4], vertices[5], vertices[7], vertices[6]], // Top
+    [vertices[0], vertices[2], vertices[6], vertices[4]], // Left
+    [vertices[1], vertices[3], vertices[7], vertices[5]], // Right
+  ]
+    .sort(sortMax('x'))
+    .sort(sortMax('y'))
+    .sort(sortMax('z', true))
+
+  const vertexShader = vertex => ({
+    x: vertex.x + (vertex.z * ((1 / 2) * Math.cos(0.5))),
+    y: vertex.y + (vertex.z * ((1 / 2) * Math.sin(0.5))),
+  })
+
+  const toWorld = vertex => ({
+    x: vertex.x * (size / 2),
+    y: vertex.y * (size / 2) * -1,
+  })
+
+  ctx.translate(canvas.width / 2, canvas.height / 2)
+  ctx.fillStyle = 'white'
+
+  faces.forEach((face) => {
     ctx.beginPath()
-    ctx.moveTo(points[0].x, points[0].y)
-    points.forEach(v => ctx.quadraticCurveTo(v.cX, v.cY, v.x, v.y))
-    // ctx.closePath()
 
-    ctx.fillStyle = 'lightgreen'
-    ctx.fill()
+    face.forEach((vertex, index) => {
+      const transformed = vertexShader(vertex)
+      const world = toWorld(transformed)
+      if (index === 0) {
+        ctx.moveTo(world.x, world.y)
+      } else {
+        ctx.lineTo(world.x, world.y)
+      }
+    })
+
+    ctx.closePath()
     ctx.stroke()
-  }
-
-  const leaf = leafPolygon()
-  drawPoly(leaf)
+    ctx.fill()
+  })
 }
 
 draw()
