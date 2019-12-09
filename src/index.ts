38aa14fa737ddf861df5cbde7199f8dc108a6a5b
Chris Atkin <atkinchris@gmail.com>
2019-12-09 08:47:50 +0000

    Draw triangles between points

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import { SVG } from '@svgdotjs/svg.js'
+import Delaunator from 'delaunator'
 
 import poisson, { Point } from './poisson'
 
 const WIDTH = 600
 const HEIGHT = 600
 const BLEED = 100
-const MINIMUM_DISTANCE = 40
+const MINIMUM_DISTANCE = 60
 const K = 30
-const DEBUG = true
-const POINT_RADIUS = 1
 
 const container = document.getElementById('drawing')!
 const svg = SVG()
@@ -16,37 +15,51 @@ const svg = SVG()
   .viewbox(BLEED, BLEED, WIDTH, HEIGHT)
   .addTo(container)
 
-const drawPoint = ({ x, y }: Point, showMinimumDistance = false) => {
-  svg
-    .circle(POINT_RADIUS * 2)
-    .move(x - POINT_RADIUS, y - POINT_RADIUS)
-    .fill('blue')
-
-  if (showMinimumDistance) {
-    svg
-      .circle(MINIMUM_DISTANCE * 2)
-      .move(x - MINIMUM_DISTANCE, y - MINIMUM_DISTANCE)
-      .stroke('rgba(0, 0, 255, 0.2)')
-      .fill('none')
-  }
-}
-
 const points: Point[] = []
 const nextPoint = poisson({
-  width: WIDTH + BLEED,
-  height: HEIGHT + BLEED,
+  width: WIDTH + BLEED * 2,
+  height: HEIGHT + BLEED * 2,
   minimumDistance: MINIMUM_DISTANCE,
   k: K,
 })
 
-const allPoints = (debug = false) => {
+const allPoints = () => {
   const point = nextPoint()
 
-  if (point === null) return
-
-  drawPoint(point, debug)
+  if (point === null) return Promise.resolve()
   points.push(point)
-  requestAnimationFrame(() => allPoints(debug))
+
+  allPoints()
 }
 
-allPoints(DEBUG)
+allPoints()
+
+type TuplePoint = [number, number]
+
+const pointsAsTuples: TuplePoint[] = points.map(point => [point.x, point.y])
+const delauney = Delaunator.from(pointsAsTuples).triangles
+const triangles: Array<[TuplePoint, TuplePoint, TuplePoint]> = []
+
+for (let i = 0; i < delauney.length; i += 3) {
+  triangles.push([pointsAsTuples[delauney[i]], pointsAsTuples[delauney[i + 1]], pointsAsTuples[delauney[i + 2]]])
+}
+
+let index = 0
+
+const drawTriangle = () => {
+  if (index >= triangles.length) {
+    return
+  }
+
+  const triangle = triangles[index]
+
+  index += 1
+  svg
+    .polygon(triangle)
+    .fill('none')
+    .stroke({ width: 1, color: 'rgba(64,64,64,1)' })
+
+  requestAnimationFrame(drawTriangle)
+}
+
+drawTriangle()
